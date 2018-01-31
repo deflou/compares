cb9c1da792bf2a0e0fb382b2d4944b56ef0d77a6
funcraft <aivanov@fix.ru>
2018-01-31 12:55:32 +0300

    compare version

--- src/components/compares/CompareVersion.php
+++ src/components/compares/CompareVersion.php
@@ -1,4 +1,23 @@
 <?php
 namespace deflou\components\compares;
 
-class CompareVersion extends CompareAbstract{}
+/**
+ * Class CompareVersion
+ *
+ * @package deflou\components\compares'
+ * @author deflou.dev@gmail.com
+ */
+class CompareVersion extends CompareAbstract
+{
+    /**
+     * @param $current
+     * @param $compareTo
+     * @param string $modification
+     *
+     * @return bool|mixed
+     */
+    public function compare($current, $compareTo, $modification = '')
+    {
+        return version_compare($current, $compareTo, $modification);
+    }
+}
